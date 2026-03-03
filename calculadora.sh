70f1e96b9dedc615c5917fc6a5712053e129fdfd
filipe oliveira <felipe300oliveiraoliveira@gmail.com>
2026-03-03 17:47:51 -0300

    Add basic arithmetic operations to calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -1,1 +1,13 @@
-
+echo "digite o primeiro numero:"
+read num1
+echo "digite o segundo numero:"
+read num2
+# soma
+soma-$ (echo "$num1 + $num2" | bc)
+echo "soma: $soma"
+# subtrac
+subtracao-$(echo "$num1 - $num2" | bc)
+echo "subtracao: $subtracao"
+#multiplicaE
+multiplicacao-$(echo "$num1 * $num2" | bc)
+echo "multiplicacao: $multiplicacao"
