8ab9318a30898fb2cfab9565dc05345d07527a28
filipe oliveira <felipe300oliveiraoliveira@gmail.com>
2026-03-03 18:18:48 -0300

    Update calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -5,9 +5,9 @@ read num2
 # soma
 soma-$ (echo "$num1 + $num2" | bc)
 echo "soma: $soma"
-# subtrac
-subtracao-$(echo "$num1 - $num2" | bc)
-echo "subtracao: $subtracao"
-#multiplicaE
-multiplicacao-$(echo "$num1 * $num2" | bc)
-echo "multiplicacao: $multiplicacao"
+# subtração
+subtração-$(echo "$num1 - $num2" | bc)
+echo "subtração: $subtracao"
+#multiplicação
+multiplicação-$(echo "$num1 * $num2" | bc)
+echo "multiplicação: $multiplicacao"
